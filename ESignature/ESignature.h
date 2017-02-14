6e047ce8ae28e8655cd5ced62ae3db3d40092a99
Yoga Widyakrisna <yoga.widyakrisna@gmail.com>
2017-02-14 16:16:45 +0700

    Initial commit for library and sample app

---
+++ ESignature/ESignature.h
@@ -0,0 +1,19 @@
+//
+//  ESignature.h
+//  ESignature
+//
+//  Created by Yoga Widyakrisna on 2/14/17.
+//  Copyright © 2017 Icehouse. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ESignature.
+FOUNDATION_EXPORT double ESignatureVersionNumber;
+
+//! Project version string for ESignature.
+FOUNDATION_EXPORT const unsigned char ESignatureVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ESignature/PublicHeader.h>
+
+
---
+++ SampleApp/SampleApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SampleApp
+//
+//  Created by Yoga Widyakrisna on 2/14/17.
+//  Copyright © 2017 Icehouse. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
